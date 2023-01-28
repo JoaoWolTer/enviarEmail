bfb2e8b2c14c89ec2726e8b0a33a46324d8f018d
Wolter <Wolter@DESKTOP-KPTQ5AE>
2023-01-28 00:27:35 -0200

    Codigo ok, problema com Gmail

--- enviando-email/src/test/java/enviando_email/enviando_email/AppTest.java
+++ enviando-email/src/test/java/enviando_email/enviando_email/AppTest.java
@@ -1,6 +1,15 @@
 package enviando_email.enviando_email;
 
+import java.util.Properties;
 
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Unit test for simple App.
@@ -18,18 +27,29 @@ public class AppTest {
 			Properties properties = new Properties();
 			properties.put("mail.smtp.auth", "true");
 			properties.put("mail.smtp.starttls", "true");
-			properties.put("mail.smtp.host", "stmp.gmail.com");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
 			properties.put("mail.smtp.port", "465");
 			properties.put("mail.smtp.socketFactory.port", "465");
 			properties.put("mail.smtp.socketFactory.clas", "javax.net.ssl.SSLSocketFactory");
-			
-		Sessi
-			
-			
-			
-			
-			System.out.println(senha);
-			
+
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, senha);
+				}
+			});
+
+			System.out.println("teste");
+
+			Address[] toUser = InternetAddress.parse("joaowoltermail@gmail.com");
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(userName));
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			message.setSubject("Chegou email teste");
+			message.setText("Corpo do email");
+
+			Transport.send(message);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 
