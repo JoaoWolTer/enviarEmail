86547e4d32bcaca1c703306a6257b48eb506580c
Wolter <Wolter@DESKTOP-KPTQ5AE>
2023-02-07 21:19:56 -0200

    Configurei pra enviar o PDF, mas ta enviando SEM O PDF.

--- enviando-email/src/main/java/enviando_email/enviando_email/App.java
+++ enviando-email/src/main/java/enviando_email/enviando_email/App.java
@@ -1,9 +1,6 @@
 package enviando_email.enviando_email;
 
-/**
- * Hello world!
- *
- */
+ 	
 public class App 
 {
     public static void main( String[] args )
--- enviando-email/src/test/java/enviando_email/enviando_email/AppTest.java
+++ enviando-email/src/test/java/enviando_email/enviando_email/AppTest.java
@@ -1,60 +1,26 @@
 package enviando_email.enviando_email;
 
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-	private String userName = "joaowoltermail@gmail.com";
-	private String senha = "snjkfvjkbhgfcjmq";
-
-	@org.junit.Test
-	public void testeEmail() {
-
-		try {
-
-			Properties properties = new Properties();
-			properties.put("mail.smtp.auth", "true");
-			properties.put("mail.smtp.starttls", "true");
-			properties.put("mail.smtp.host", "smtp.gmail.com");
-			properties.put("mail.smtp.port", "465");
-			properties.put("mail.smtp.socketFactory.port", "465");
-			properties.put("mail.smtp.socketFactory.clas", "javax.net.ssl.SSLSocketFactory");
-
-			Session session = Session.getInstance(properties, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(userName, senha);
-				}
-			});
-
-			System.out.println("teste");
-
-			Address[] toUser = InternetAddress.parse("joaowoltermail@gmail.com");
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName));
-			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("Chegou email teste");
-			message.setText("Corpo do email");
-
-			Transport.send(message);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-
+@org.junit.Test
+	public void testeEmail() throws Exception{
+		
+		StringBuilder stringBuilderTextoEmail = new StringBuilder();
+		
+	stringBuilderTextoEmail.append("Boa noite! <br/><br/>");
+	stringBuilderTextoEmail.append("Tudo bem?! Espero que sim! <br/><br/>");
+	stringBuilderTextoEmail.append("Clique no link!<br/><br/>");
+	stringBuilderTextoEmail.append("<a target=\"_blank\" href=\"https://www.netflix.com/browse\" style=\" color:#0D9240; padding: 14px 25px; text-align:center; text-decoration: none; display:inline-block; border-radius:30px; fonte-size:20px; font-family:courier; border: 3px solid green; background-color:#9BEDBA; \"><b> Clique aqui!<b/></a>");
+	
+	
+		ObjetoEnviaEmail enviaEmail = 
+						new ObjetoEnviaEmail(
+						"joaowoltermail@gmail.com",
+						"WolTer", 
+						"Assunto email",
+						stringBuilderTextoEmail.toString());
+		
+		enviaEmail.enviarEmail(true);
+		
 	}
 
 }
